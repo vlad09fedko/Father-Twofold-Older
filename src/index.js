29f8d0ba2cfb841f903a0235a69bd1f8b1cf87ce
Vlad <vlad09fedko@gmaul.com>
2026-04-16 16:03:26 +0300

    Fix bugs2

--- src/index.js
+++ src/index.js
@@ -20,21 +20,22 @@ let calculateDifferenceIn15Years = (fatherAge, sonAge) => {
   return numberYears;
 };
 
-let FATHER_AGE = 50;
-let SON_AGE = 30;
+const FATHER_AGE = 50;
+const SON_AGE = 30;
 let numberYears = false;
-if (typeof FATHER_AGE !== 'number' || typeof SON_AGE !== 'number') {
-  console.log('Error! Invalid data type entered.');
+if (
+  typeof FATHER_AGE !== 'number' ||
+  typeof SON_AGE !== 'number' ||
+  FATHER_AGE < SON_AGE ||
+  FATHER_AGE - SON_AGE < 15 ||
+  FATHER_AGE < 15 ||
+  SON_AGE < 0
+) {
+  console.log('Error!');
 } else {
   numberYears = calculateDifferenceIn15Years(FATHER_AGE, SON_AGE);
 }
-if (FATHER_AGE < SON_AGE) {
-  console.log("Error! The son's age cannot be greater than the father's age.");
-} else if (FATHER_AGE - SON_AGE < 15) {
-  console.log('Error! The age difference must be at least 15 years.');
-} else if (FATHER_AGE < 0 || SON_AGE < 0) {
-  console.log('Error! Age cannot be negative.');
-} else if (numberYears > 0) {
+if (numberYears > 0) {
   console.log(
     `Father will be twice as old as his son in ${numberYears} ${numberYears === 1 ? 'year' : 'years'}`,
   );
