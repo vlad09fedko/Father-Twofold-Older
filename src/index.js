c0bfd7a43ccc2c107d33b070243c5c7d80cb8fc0
Vlad <vlad09fedko@gmaul.com>
2026-04-16 22:36:41 +0300

    Fix bugs3

--- src/index.js
+++ src/index.js
@@ -1,48 +1,38 @@
 'use strict';
+
 /**
  *
  * @param {number} fatherAge
  * @param {number} sonAge
  * @returns {number} numberYears
  */
-let calculateDifferenceIn15Years = (fatherAge, sonAge) => {
-  let numberYears = 0;
-  if (fatherAge / sonAge >= 2) {
-    while ((fatherAge + numberYears) / 2 !== sonAge + numberYears) {
-      numberYears++;
-    }
-  } else {
-    while ((fatherAge + numberYears) / 2 !== sonAge + numberYears) {
-      numberYears--;
-    }
+const getYearsToDoubleAge = (fatherAge, sonAge) => {
+  if (fatherAge - sonAge < 15 || sonAge < 0) {
+    return 9999;
   }
 
-  return numberYears;
+  return fatherAge - 2 * sonAge;
 };
 
-const FATHER_AGE = 50;
-const SON_AGE = 30;
-let numberYears = false;
-if (
-  typeof FATHER_AGE !== 'number' ||
-  typeof SON_AGE !== 'number' ||
-  FATHER_AGE < SON_AGE ||
-  FATHER_AGE - SON_AGE < 15 ||
-  FATHER_AGE < 15 ||
-  SON_AGE < 0
-) {
+const FATHER_AGE = 60;
+const SON_AGE = 35;
+let numberYears = getYearsToDoubleAge(FATHER_AGE, SON_AGE);
+
+if (numberYears === 9999) {
   console.log('Error!');
-} else {
-  numberYears = calculateDifferenceIn15Years(FATHER_AGE, SON_AGE);
+}
+
+if (numberYears === 0) {
+  console.log('The father is now twice as old as his son.');
 }
 if (numberYears > 0) {
   console.log(
     `Father will be twice as old as his son in ${numberYears} ${numberYears === 1 ? 'year' : 'years'}`,
   );
-} else if (numberYears < 0) {
+}
+if (numberYears < 0) {
+  numberYears = Math.abs(numberYears);
   console.log(
-    `Father was twice as old as his son ${Math.abs(numberYears)} ${Math.abs(numberYears) === 1 ? 'year' : 'years'} ago`,
+    `Father was twice as old as his son ${numberYears} ${numberYears === 1 ? 'year' : 'years'} ago`,
   );
-} else if (numberYears === 0) {
-  console.log('The father is now twice as old as his son.');
 }
